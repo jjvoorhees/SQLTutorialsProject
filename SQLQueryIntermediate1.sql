2fea6da0b3b75809111bc6d837c0d921781a9629
jjvoorhees <115953086+jjvoorhees@users.noreply.github.com>
2022-10-20 16:50:04 -0400

    Update SQLQueryIntermediate1.sql
    Updated my file!

--- SQLQueryIntermediate1.sql
+++ SQLQueryIntermediate1.sql
@@ -37,6 +37,37 @@ Inner Join EmployeeSalary
 WHERE JobTitle = 'Salesman'
 GROUP BY JobTitle
 
+--Insert into EmployeeDemographics VALUES
+--(1011, 'Ryan', 'Howard', 26, 'Male'),
+--(NULL, 'Holly', 'Flax', NULL, NULL),
+--(1013, 'Darryl', 'Philbin', NULL, 'Male')
 
+--Create Table WareHouseEmployeeDemographics 
+--(EmployeeID int, 
+--FirstName varchar(50), 
+--LastName varchar(50), 
+--Age int, 
+--Gender varchar(50)
+--)
+
+--INSERT INTO WareHouseEmployeeDemographics VALUES
+--(1013, 'Darryl', 'Philbin', NULL, 'Male'),
+--(1050, 'Roy', 'Anderson', 31, 'Male'),
+--(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+--(1052, 'Val', 'Johnson', 31, 'Female')
+
+--DELETE FROM EmployeeDemographics WHERE FirstName = 'Holly';
+
+--SELECT *
+--FROM SQLTutorial.dbo.EmployeeDemographics
+--Full Outer Join SQLTutorial.dbo.WareHouseEmployeeDemographics
+--	ON EmployeeDemographics.EmployeeID = WareHouseEmployeeDemographics.EmployeeID
+
+SELECT EmployeeID, FirstName, Age
+FROM SQLTutorial.dbo.EmployeeDemographics
+UNION
+SELECT EmployeeID, JobTitle, Salary
+FROM SQLTutorial.dbo.EmployeeSalary
+ORDER BY EmployeeID
 
 
