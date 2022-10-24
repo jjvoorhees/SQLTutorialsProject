bc14882e96de302ac8fb8219e3d4da88af4c8d49
jjvoorhees <115953086+jjvoorhees@users.noreply.github.com>
2022-10-23 22:55:25 -0400

    Update SQLQueryIntermediate1.sql
    Learned about Case Statements!

--- SQLQueryIntermediate1.sql
+++ SQLQueryIntermediate1.sql
@@ -70,4 +70,25 @@ SELECT EmployeeID, JobTitle, Salary
 FROM SQLTutorial.dbo.EmployeeSalary
 ORDER BY EmployeeID
 
+--SELECT FirstName, LastName, Age,
+--CASE
+--	WHEN Age = 69 THEN 'Doofus'
+--	WHEN Age > 30 THEN 'Old'
+--	ELSE 'Baby'
+--END
+--FROM SQLTutorial.dbo.EmployeeDemographics
+--WHERE Age is NOT NULL
+--ORDER BY Age
+
+SELECT FirstName, LastName, JobTitle, Salary,
+CASE
+	WHEN JobTitle = 'Salesman' THEN Salary + (Salary * .10)
+	WHEN JobTitle = 'Accountant' THEN Salary + (Salary * .05)
+	WHEN JobTitle = 'HR' THEN Salary + (Salary * .000001)
+	ELSE Salary + (Salary * .03)
+END AS SalaryAfterRaise
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
 
